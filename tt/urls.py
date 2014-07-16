a9ce588dc003581240378bfb3fab73308456b040
Dmitrii Sitnikov <a@ptls.ru>
2014-07-16 16:05:26 +0400

    Abonents Results modified

--- tt/urls.py
+++ tt/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = patterns('',
-#     url(r'^add/$','notes.views.note_add', name='note_add'),
+    # url(r'^add/$','tt.views.tt_add', name='tt_add'),
 #     url(r'^(?P<note_id>\d+)/del/$','notes.views.note_del', name='note_del'),
     url(r'^all/$', 'tt.views.tt_all', name='tt_all'),
 )
