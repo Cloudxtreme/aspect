9bcb2f7faee0ccda55fcb85fd7c283606f653721
Dmitrii Sitnikov <a@ptls.ru>
2014-07-18 15:24:16 +0400

    -

--- aspekt/settings.py
+++ aspekt/settings.py
@@ -154,9 +154,9 @@ TEMPLATE_DIRS = (
 
 # STATIC = ''
 
-SERIALIZATION_MODULES = {
-    'json': 'wadofstuff.django.serializers.json'
-}
+# SERIALIZATION_MODULES = {
+#     'json': 'wadofstuff.django.serializers.json'
+# }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
