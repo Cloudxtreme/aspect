2aa0620ad6e2dd3df8c2d840f7e165f47198de6b
Dmitrii Sitnikov <a@ptls.ru>
2014-08-27 15:31:52 +0400

    Monthly fees edited

--- monthly_fee.py
+++ monthly_fee.py
@@ -8,13 +8,20 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'aspekt.settings'
 
 from pays.models import WriteOff, WriteOffType
 from users.models import Service
-from datetime import datetime
+import datetime
 import locale
 
 locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 wot = WriteOffType.objects.get(title='Абонентская плата')
-comment = 'Абонентская плата за ' + datetime.today().strftime('%B %Y')
+today = datetime.date.today()
+this_month = today.strftime('%B %Y')
+last_month = (today - datetime.timedelta(days=1)).strftime('%B %Y')
+descr = 'Абонентская плата за '
 
 for item in Service.objects.filter(status='A'):
-    write_off = WriteOff(abonent=item.abon, service=item, wot=wot,summ=item.plan.price, date=datetime.now(), comment=comment)
+    if item.abon.is_credit == 'O':
+        comment = descr + last_month
+    else:
+        comment = descr + this_month
+    write_off = WriteOff(abonent=item.abon, service=item, wot=wot,summ=item.plan.price, date=datetime.datetime.now(), comment=comment)
     write_off.save()
