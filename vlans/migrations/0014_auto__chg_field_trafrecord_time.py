a856f4d984d6c3c0eb543057604d7757d8c4e892
Dmitrii Sitnikov <a@ptls.ru>
2016-02-12 10:10:01 +0300

    +

---
+++ vlans/migrations/0014_auto__chg_field_trafrecord_time.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'TrafRecord.time'
+        db.alter_column(u'vlans_trafrecord', 'time', self.gf('django.db.models.fields.DateTimeField')())
+
+    def backwards(self, orm):
+
+        # Changing field 'TrafRecord.time'
+        db.alter_column(u'vlans_trafrecord', 'time', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, auto_now_add=True))
+
+    models = {
+        u'users.segment': {
+            'Meta': {'object_name': 'Segment'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'vlans.ipaddr': {
+            'Meta': {'ordering': "['decip']", 'object_name': 'IPAddr'},
+            'decip': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip': ('django.db.models.fields.IPAddressField', [], {'unique': 'True', 'max_length': '15'}),
+            'net': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['vlans.Network']"})
+        },
+        u'vlans.location': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Location'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'bs_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'comment': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'geolocation': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'rent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['vlans.Rent']", 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'vlans.network': {
+            'Meta': {'ordering': "['decip']", 'unique_together': "(('ip', 'mask'),)", 'object_name': 'Network'},
+            'decip': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_dhcpd': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'ip': ('django.db.models.fields.IPAddressField', [], {'max_length': '15'}),
+            'mask': ('django.db.models.fields.IntegerField', [], {'max_length': '2'}),
+            'net_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['vlans.Network']"}),
+            'segment': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.Segment']"}),
+            'vlan': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['vlans.Vlan']", 'null': 'True', 'blank': 'True'})
+        },
+        u'vlans.node': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Node'},
+            'bs_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latlng': ('django.db.models.fields.CharField', [], {'max_length': '300', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'vlans.rent': {
+            'Meta': {'object_name': 'Rent'},
+            'cost': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'vlans.trafrecord': {
+            'Meta': {'ordering': "['time']", 'object_name': 'TrafRecord'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inbound': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'interval': ('django.db.models.fields.PositiveIntegerField', [], {'default': '300'}),
+            'ip': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['vlans.IPAddr']"}),
+            'octets': ('django.db.models.fields.BigIntegerField', [], {}),
+            'time': ('django.db.models.fields.DateTimeField', [], {})
+        },
+        u'vlans.vlan': {
+            'Meta': {'ordering': "['number']", 'object_name': 'Vlan'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.IntegerField', [], {'unique': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['vlans']
